7ea8df4ce482fbd656ed326780036018ee45d222
kojima <canonn301@gmail.com>
2017-01-31 20:58:11 +0900

    結合問題終了

--- funish12.py
+++ funish12.py
@@ -3,5 +3,7 @@ from pandas import Series,DataFrame
 import pandas as pd
 
 df = pd.read_csv("funish.csv")
-
-print(df.insert())
+df2 = pd.read_csv("funish_team.csv")
+print(df2)
+df3 = pd.merge(df,df2,left_on="team",right_on="team_no",how="inner",suffixes=("_x","_y"))
+print(df3)
